97e7754d5a51505e1ca13f831640d86a4cffef5f
Dorian Bweta <dorianbweta@gmail.com>
2023-05-22 14:19:43 +0800

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @anwser = ''
+    if params[:question] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:question].include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
