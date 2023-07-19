26ff0321ea2b71b573683447bbd7d58ad2500855
Dorian Bweta <dorianbweta@gmail.com>
2023-07-19 16:24:31 +0800

    front + back done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,10 +2,14 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def answer
-    @anwser = ''
-    if params[:question] == 'I am going to work'
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    # Match a regular expression (regex) pattern against a string.
+    # The 'i' at the end makes it a case-insensitive match.
+    elsif @question =~ /i am going to work/i
       @answer = 'Great!'
-    elsif params[:question].include?('?')
+    elsif @question.ends_with?('?')
       @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get 'ask', to: 'questions#ask'
-  get 'answer', to: 'questions#answer'
+
+  # The routes map incoming requests to controller actions,
+  # which in turn interact with the model layer and render the appropriate view
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
 end
